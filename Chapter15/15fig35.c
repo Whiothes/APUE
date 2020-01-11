7b25d52062bbfad28ae79e03885a289b8028628a
whiothes <whiothes81@gmail.com>
2020-01-11 22:06:08 +0800

    up to section 15.11

--- Chapter15/15fig35.c
+++ Chapter15/15fig35.c
@@ -1,9 +1,16 @@
-#include "slock.h"
-
+/**
+ *   @file     15fig35.c
+ *   @date     2020-01-11
+ *   @author   whiothes <whiothes81@gmail.com>
+ *   @version  1.0
+ *   @brief    Mutual exclusion using a POSIX semaphore
+ */
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#include "slock.h"
 
 struct slock *s_alloc() {
     struct slock *sp;
@@ -16,16 +23,14 @@ struct slock *s_alloc() {
     do {
         snprintf(sp->name, sizeof(sp->name), "/%ld.%d", (long)getpid(), cnt++);
         sp->semp = sem_open(sp->name, O_CREAT | O_EXCL, S_IRWXU, 1);
-
-    } while ((sp->semp == SEM_FAILED) && errno == EEXIST);
-
+    } while ((sp->semp == SEM_FAILED) && (errno == EEXIST));
     if (sp->semp == SEM_FAILED) {
         free(sp);
         return NULL;
     }
 
     sem_unlink(sp->name);
-    return (sp);
+    return sp;
 }
 
 void s_free(struct slock *sp) {
@@ -33,10 +38,8 @@ void s_free(struct slock *sp) {
     free(sp);
 }
 
-int s_lock(struct slock *sp) { return (sem_wait(sp->semp)); }
+int s_lock(struct slock *sp) { return sem_wait(sp->semp); }
 
-int s_trylock(struct slock *sp) { return (sem_trywait(sp->semp)); }
+int s_trylock(struct slock *sp) { return sem_trywait(sp->semp); }
 
-int s_unlock(struct slock *sp) { return (sem_post(sp->semp)); }
-
-int main() {}
+int s_unlock(struct slock *sp) { return sem_post(sp->semp); }
