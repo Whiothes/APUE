ce25b60a5996e1af893b2591d457eaa5b3360d49
whiothes <whiothes81@gmail.com>
2020-01-22 09:49:04 +0800

    update exercises' name

--- rename.py
+++ rename.py
@@ -7,12 +7,13 @@ import regex
 for root, dirs, files in os.walk(
         "/Users/zhoush/Documents/Notes/books/system/APUE"):
     for filename in files:
-        x = regex.findall("fig([0-9]+)\\.([0-9]+)\\.c$", filename)
+        x = regex.findall("(fig|ex)([0-9]+)\\.([0-9]+)\\.c$", filename)
         if x:
             y = list(x[0])
-            y[0] = y[0].zfill(2)
+            y[0] = y[0]
             y[1] = y[1].zfill(2)
+            y[2] = y[2].zfill(2)
             src = os.path.join(root, filename)
-            dst = os.path.join(root, y[0] + "fig" + y[1] + ".c")
-            print(src + " ->" + dst)
+            dst = os.path.join(root, y[1] + y[0] + y[2] + ".c")
+            print(src + " =>" + dst)
             os.rename(src, dst)
